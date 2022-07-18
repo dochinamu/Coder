58e50d8439b8f345f3472ac9d0a58324e30a223f
2018124058 <2018124058@yonsei.ac.kr>
2022-07-18 20:06:16 +0900

    유저모델 정의, 로그인, 회원가입, 마이페이지, 로비 생성

--- account_app/views.py
+++ account_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import auth
-from django.contrib.auth.models import User
-#from .models import User
+#from django.contrib.auth.models import User
+from .models import User
 
 # Create your views here.
 def account(request):
