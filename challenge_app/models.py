0d792a9161a0d314786d7f9f9391bcea4543c844
0921sean <98502180+0921sean@users.noreply.github.com>
2022-08-03 19:47:42 +0900

    [08.03 승범] 챌린지 등록, 참여, 진행상황, 마이챌린지 구현

--- challenge_app/models.py
+++ challenge_app/models.py
@@ -5,14 +5,15 @@ from account_app.models import User
 class Challenge(models.Model):
     category = models.CharField(max_length=15, default='')
     title = models.CharField(max_length=30)
-    body = models.TextField()
+    body = models.TextField(default='')
+    tag = models.TextField(default='')
     #started_at = models.DateTimeField(auto_now_add=True)
-    participant = models.ManyToManyField(User, through="ChallengeUsers")
+    participant = models.ManyToManyField(User, through="ChallengeUsers", related_name="challenges")
     
     def __str__(self):
         return self.title
 
 class ChallengeUsers(models.Model):
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    participant = models.ForeignKey(Challenge, on_delete=models.CASCADE)
     started_at = models.DateTimeField(auto_now_add=True)
--- challenge_app/urls.py
+++ challenge_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('python_challenge', python_challenge, name="python_challenge"),
-    path('python_challenge1', python_challenge1, name="python_challenge1"),
+    path('new_challenge', new_challenge, name="new_challenge"),
+    path('my_challenge', my_challenge, name="my_challenge"),
+    path('python_explain/<int:chal_id>', python_explain, name="python_explain"),
+    path('python_challenge/<int:chal_id>/', python_challenge, name="python_challenge"),
 ]
